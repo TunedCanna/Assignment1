ffa5bca5814efe8991a9c978813adc0be734c0d0
Tuned Canna <you@example.com>
2021-09-08 00:10:17 +0800

    Lots more work again

--- app/src/main/java/MAD/assignment1/model/Practical.kt
+++ app/src/main/java/MAD/assignment1/model/Practical.kt
@@ -3,5 +3,18 @@ package MAD.assignment1.model
 class Practical(
     var title: String,
     var description: String,
-    var marks: Double) {
+    var availableMarks: Double
+) {
+
+    var finalMarks = -1.0
+
+    fun isMarked(): Boolean = finalMarks >= 0
+
+    fun getMarkString(): String {
+        return if (isMarked()) {
+            "$finalMarks/$availableMarks"
+        } else {
+            "TBM"
+        }
+    }
 }
